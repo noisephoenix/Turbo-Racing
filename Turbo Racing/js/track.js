b9435e251af51da30c05a131e90372313a1b0a46
Gavin Phoenix <gavin.phoenix@gmail.com>
2019-03-27 19:04:14 +0000

    Optimise image loading
    Turn the image loading into an array to easy adding new images and tile types

--- Turbo Racing/js/track.js
+++ Turbo Racing/js/track.js
@@ -64,25 +64,8 @@ function drawTracks() {
 
             var arrayIndex = rowColtoArrayIndex(eachCol, eachRow);
             var tileKindHere = trackGrid[arrayIndex];
-            var useImg;
-            switch (tileKindHere) {
-                case TRACK_ROAD:
-                    useImg = roadPic;
-                    break;
-                case TRACK_WALL:
-                    useImg = wallPic;
-                    break;
-                case TRACK_GOAL:
-                    useImg = goalPic;
-                    break;
-                case TRACK_TREE:
-                    useImg = treePic;
-                    break;
-                case TRACK_FLAG:
-                    useImg = flagPic;
-                    break;
-
-            }// end of this track here
+            var useImg = trackPics[tileKindHere];
+            
             canvasContext.drawImage(useImg, TRACK_W * eachCol, TRACK_H * eachRow);
         } // end of for each col
     } //end of for each row
